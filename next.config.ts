2457ecbd08d1076c76a0d040720f2ef459135ecd
lijiahao <2661247098@qq.com>
2026-05-26 18:27:13 +0800

    修改部署后css无法访问问题

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
-import type { NextConfig } from "next";
+import 输入 { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
+  basePath: isProd ? "/lijiahao-portfolio" : "",
+
+  assetPrefix: isProd ? "/lijiahao-portfolio/" : "",
 };
 
 export default nextConfig;
